ff8052b5d53f46eacde2e66e279b50a13861e8d7
Richard Tom <tomrichard14@gmail.com>
2018-06-18 10:12:55 -0700

    update store to create,update, delete v1: issues with findByID

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* global store, cuid */
+/* global store, cuid,$ Item*/
 
 // eslint-disable-next-line no-unused-vars
 const shoppingList = (function(){
--- scripts/store.js
+++ scripts/store.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/* global store cuid, Item*/
 const store = (function () {
   const items = [
     { id: cuid(), name: 'apples', checked: false },
@@ -9,6 +9,33 @@ const store = (function () {
   ];
   const hideCheckedItems = false;
   const searchTerm = '';
+  const findById = function(id) {
+    return items.find(id => items.id === id);
+  };
+  const addItem = function (name) {
+    try {
+      Item.validateName(name);
+      items.push(Item.create(name));
+    }
+    catch(error) {
+      console.error(`Cannot add item: ${error.message}`);
+    }
+  };
+  const findAndToggleChecked = function(id) {
+    this.findById(id).checked = !items.checked;
+  };
+  const findAndUpdateName = function(id, newName) {
+    try {
+      Item.validateName(newName);
+      this.findById(id).name = newName;
+    }
+    catch (error) {
+      console.error(`Cannot update name: ${error.message}`);
+    }
+  };
+  const findAndDelete = function(id) {
+    this.items.filter(item => findById(id) !== item.id);
+  };
 
-  return {items, hideCheckedItems, searchTerm};
+  return {items, hideCheckedItems, searchTerm, addItem, findAndToggleChecked, findAndUpdateName, findAndDelete, findById};
 }() );
